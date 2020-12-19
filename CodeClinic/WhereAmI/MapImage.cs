580ed1f4670a826173742597db5a498108d2b98c
Akimi Yano <v-akimiyano@microsoft.com>
2020-12-18 18:21:55 -0800

    Added Code Clinic

---
+++ CodeClinic/WhereAmI/MapImage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Device.Location;
+using System.Diagnostics;
+using System.Net;
+
+namespace WhereAmI
+{
+    class MapImage
+    {
+        public static void Show(GeoCoordinate location)
+        {
+            string filename = $"{location.Latitude:##.000},{location.Longitude:##.000},{location.HorizontalAccuracy:####}m.jpg";
+
+            DownloadMapImage(BuildURI(location), filename);
+
+            OpenWithDefaultApp(filename);
+        }
+
+        private static void DownloadMapImage(Uri target, string filename)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(target, filename);
+            }
+        }
+
+        /// <summary>
+        /// Map Image REST API by HERE Location Services
+        /// </summary>
+        /// <remarks>
+        /// https://developer.here.com/
+        /// </remarks>
+        private static Uri BuildURI(GeoCoordinate location)
+        {
+
+            // Get the API key from https://developer.here.com/documentation/authentication/dev_guide/index.html
+            string API_KEY = "";
+           
+
+            //var HereApi_DNS = "image.maps.cit.api.here.com";
+            var HereApi_DNS = "image.maps.ls.hereapi.com";
+            var HereApi_URL = $"https://{HereApi_DNS}/mia/1.6/mapview";
+
+            var latlon = $"&lat={location.Latitude}&lon={location.Longitude}";
+
+            return new Uri(HereApi_URL + $"?apiKey = {API_KEY}&u={location.HorizontalAccuracy}"  + latlon);
+        }
+
+        private static void OpenWithDefaultApp(string filename)
+        {
+            var si = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C start {filename}",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(si);
+        }
+    }
+}
---
+++ CodeClinic/WhereAmI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Device.Location;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting GeoCoordinate Watcher...");
+
+            // 1. Add a Reference to System.Device.dll - Done ( to access geocoordinate watcher class)
+
+            // 2. Use the GeoCoordinate Watcher
+            var watcher = new GeoCoordinateWatcher();
+            watcher.StatusChanged += (s, e) =>
+            {
+                Console.WriteLine($"GorCoordinateWatcher:StatusChanged: {e.Status}");
+            };
+
+
+            watcher.PositionChanged += (s, e) =>
+            {
+                watcher.Stop();
+                Console.WriteLine($"GorCoordinateWatcher:PositionChanged: {e.Position.Location}");
+
+                // 3. Use the Map Image REST API
+                MapImage.Show(e.Position.Location);
+            };
+
+            watcher.MovementThreshold = 100;
+            
+            watcher.Start();
+
+            
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
